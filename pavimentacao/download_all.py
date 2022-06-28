17493060ece6daff5ee4c43d5a49347dc937112f
Cristiano Rodrigues <71291207+cristianorbh@users.noreply.github.com>
2022-06-28 10:52:20 -0300

    Delete download_all.py

--- pavimentacao/download_all.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# path to fold with .csv files with the coordinates per region
-path = "PATH TO /mpmg_streetview_images/coordenadas"
-
-for file in os.listdir(path):
-    region = file.split('.')[0]
-
-    os.system("python3 download_data.py " + file + " " + region)
